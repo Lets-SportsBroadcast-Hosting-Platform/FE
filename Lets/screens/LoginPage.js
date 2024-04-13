e7b5595ecf03a65df41a2ecca3ae1071dd494305
윤영서 <dbsys1120@naver.com>
2024-04-13 14:51:45 +0900

    Feat: 디렉토리/Home, LoginPage

---
+++ Lets/screens/LoginPage.js
@@ -0,0 +1,25 @@
+import { StyleSheet, Text, View,Image  } from 'react-native';
+import icons from '../assets/images/clouds.png';
+
+export default function LoginPage() {
+    return (
+        <View style={styles.container}>
+        {/* <Image source={icons} style={styles.image}/> */}
+        <Text>HELLO</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    image: {
+        width: 100,
+        height: 100,
+    },
+
+});
