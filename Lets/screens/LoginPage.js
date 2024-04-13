985bdd89de21efee09ca0e82c8e0f8d505370aa5
윤영서 <dbsys1120@naver.com>
2024-04-13 16:53:17 +0900

    feat: loginpage logo

--- Lets/screens/LoginPage.js
+++ Lets/screens/LoginPage.js
@@ -1,10 +1,12 @@
 import { StyleSheet, Text, View,Image  } from 'react-native';
+import icons from '../assets/images/logo.png';
+
 
 export default function LoginPage() {
     return (
         <View style={styles.container}>
-        {/* <Image source={icons} style={styles.image}/> */}
-        <Text>HELLO</Text>
+        <Image source={icons} style={styles.image}/>
+        <Text style={styles.text}>HELLO</Text>
         </View>
     );
 }
@@ -12,13 +14,18 @@ export default function LoginPage() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
+        backgroundColor: '#01162D',
         alignItems: 'center',
         justifyContent: 'center',
     },
     image: {
         width: 100,
         height: 100,
+        transform: [{ translateY: -70 }]
     },
+    text: {
+        color:'white',
+        transform: [{ translateY: -70 }]
+    }
 
 });
