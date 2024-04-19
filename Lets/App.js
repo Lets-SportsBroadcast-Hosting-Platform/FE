19a2a228d5a201cd58cb2087627d4eb4a830d915
노재원 <82192655+nojaewon@users.noreply.github.com>
2024-04-19 20:19:36 +0900

    Merge branch 'development' into dev_noje_sso

--- Lets/App.js
+++ Lets/App.js
@@ -3,6 +3,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './screens/Home';
 import LoginPage from './screens/LoginPage';
+import ChooseUser from './screens/ChooseUser';
+import HostAuthentication from './screens/HostAuthentication';
 const Stack = createNativeStackNavigator();
 
 export default function App() {
@@ -16,6 +18,8 @@ export default function App() {
         
         <Stack.Screen name="LoginPage" component={LoginPage} options={{headerShown:false}}/>
         {/* <Stack.Screen name="TestPage" component={TestPage} /> */}
+        <Stack.Screen name="ChooseUser" component={ChooseUser} options={{headerShown:false}}/>
+        <Stack.Screen name="HostAuthentication" component={HostAuthentication} options={{headerShown:false}}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
