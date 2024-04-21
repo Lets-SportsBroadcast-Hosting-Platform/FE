8e10cc28c44bbf9d865c353e31f838947a3380a0
노재원 <82192655+nojaewon@users.noreply.github.com>
2024-04-21 14:34:37 +0900

    feat: HostAuthentication 페이지 리스트 템플릿 작업 완료

--- Lets/screens/HostAuthentication.js
+++ Lets/screens/HostAuthentication.js
@@ -4,7 +4,7 @@ import arrowToLeft from '../assets/images/arrowToLeft.png';
 import { useNavigation } from '@react-navigation/native';
 import { TextInput, Button } from 'react-native-paper';
 
-export default function LoginPage() {
+export default function HostAuthentication() {
   const navigation = useNavigation();
 
   const arrowbuttonPress = () => {
@@ -12,12 +12,39 @@ export default function LoginPage() {
     console.log("arrowbuttonPressed");
   };
 
-  //TextInput
+  // 가게 이름 검색
+  const storeInfoList = [
+    {
+      title: '벨지움재즈카페',
+      desc: '서울 강남구 테헤란로 83길 20 건영빌딩 400호'
+    },
+    {
+      title: '재즈카페',
+      desc: '서울 종로구 평창 11길 7'
+    },
+    {
+      title: '째즈카페',
+      desc: '서울 동작구 여의대방로 24길 129'
+    },
+  ]
   const [text, setText] = React.useState('');
   const onChangeText = (inputText) => {
     setText(inputText);
-    console.log(inputText);
   };
+
+  const goNextStep=()=>{
+    // navigation.navigate('다음 스텝 컴포넌트명')
+    console.log('next step Go!')
+  }
+
+  const StoreList = storeInfoList.map((store, storeIdx)=>{
+    return (
+    <TouchableOpacity style={styles.storeItem} onPress={goNextStep} key={storeIdx}>
+      <Text style={[styles.storeItemText, styles.storeItemTitle]}>{store.title}</Text>
+      <Text style={[styles.storeItemText, styles.storeItemDesc]}>{store.desc}</Text>
+    </TouchableOpacity>
+    )
+  })
 
   return (
     <View style={styles.container}>
@@ -28,18 +55,21 @@ export default function LoginPage() {
         <Text style={styles.headerText}>사장님이신가요?</Text>
         
       </View>
+      <View style={styles.contentContainer}>
+        <View style={styles.textInputContainer}>
+          <TextInput
+            label="가게 이름을 입력해주세요"
+            value={text}
+            onChangeText={onChangeText}
+            style={styles.storeInputText}
+          />
+        </View>
+        <Button mode="contained" onPress={() => console.log('Pressed')} style={styles.typeButton}>
+          직접등록
+        </Button>
+        {StoreList}
 
-      <View style={styles.textInputContainer}>
-        <TextInput
-          label="가게 이름을 입력해주세요"
-          value={text}
-          onChangeText={onChangeText}
-          style={styles.storeInputText}
-        />
       </View>
-      <Button mode="contained" onPress={() => console.log('Pressed')} style={styles.typeButton}>
-        직접등록
-      </Button>
     </View>
   );
 }
@@ -49,7 +79,9 @@ const styles = StyleSheet.create({
     height: '100%',
     flex: 1,
     alignItems: 'center',
-    position: 'relative'
+    position: 'relative',
+    // paddingVertical: 30,
+    // paddingHorizontal: 10
   },
   header: {
     flexDirection: 'row',
@@ -77,18 +109,43 @@ const styles = StyleSheet.create({
     height: 20,
      // 이미지와 텍스트 사이의 간격 조정
   },
+  contentContainer: {
+    flex: 1,
+    width: '100%',
+    marginTop: 20,
+    paddingLeft: 20,
+    paddingRight: 20,
+    alignItems: 'center'
+  },
   textInputContainer: {
-    width: 360,
+    width: '100%',
     justifyContent: 'center',
+    borderRadius: 10,
+    borderBottomWidth: 0,
   },
   typeButton: {
-    width: 360,
-    borderRadius: 5,
-    marginTop: 7,
+    width: '100%',
+    borderRadius: 10,
+    marginTop: 15,
     backgroundColor:'#01162D',
   },
   storeInputText :{
     height:45,
-    borderRadius:5
+    backgroundColor: '#ddd',
+  },
+  storeItem: {
+    width: '100%',
+    marginTop: 15,
+    padding: 5,
+  },
+  storeItemText: {
+    width: '100%',
+  },
+  storeItemTitle: {
+    fontSize: 18,
+    fontWeight: 'bold'
+  },
+  storeItemDesc: {
+    color: '#888',
   }
 });
